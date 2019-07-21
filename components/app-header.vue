49386a64a9cfb90a614c0a3c8cb30006fdc42964
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-07-21 13:14:59 +0530

    Remove test users section as now time to build app!

--- components/app-header.vue
+++ components/app-header.vue
@@ -4,9 +4,6 @@
       <nuxt-link to="/" tag="li" exact-active-class="is-active">
         <a>Home</a>
       </nuxt-link>
-      <nuxt-link to="/users" tag="li" active-class="is-active">
-        <a>Users</a>
-      </nuxt-link>
     </ul>
   </header>
 </template>
