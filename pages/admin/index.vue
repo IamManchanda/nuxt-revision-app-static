27950e2a26d6943ed3ec1fdc87f427f6a1ef623c
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-07-23 23:21:51 +0530

    Admin Page & Create Post page UI creation!

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,5 +1,42 @@
 <template>
   <div>
-    <h1 class="is-size-3">This is my absolutely fantastic "Admin" Page!</h1>
+    <div class="posts-heading-container">
+      <h1 class="is-size-3">Existing Posts</h1>
+      <nuxt-link
+        type="button"
+        class="button is-primary"
+        to="/admin/new-post"
+      >Create Post</nuxt-link>
+    </div>
+    <section class="columns is-multiline all-posts-container">
+      <div class="column is-full" v-for="post_id of 6" :key="post_id">
+        <app-post-excerpt :post_id="post_id" title="Reprehenderit, voluptas?">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum consectetur rerum asperiores laboriosam dolores, placeat possimus ipsa, quia quasi magni inventore omnis minima nulla deserunt impedit distinctio non pariatur dignissimos a, facere delectus quaerat suscipit reprehenderit reiciendis. Quis nemo voluptate omnis nam illum adipisci ratione, non, vero soluta aliquam perspiciatis recusandae obcaecati? Repellendus id ipsa, eius tenetur dolore officia, ad, ipsum aut quidem rem ex quia sapiente a eligendi beatae accusantium aperiam odio dolorum natus! Veniam consequatur doloribus totam incidunt repellendus recusandae laudantium nemo similique dolor, ea natus nam dolore voluptatum veritatis quos quod excepturi placeat molestiae consequuntur sunt minima?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum consectetur rerum asperiores laboriosam dolores, placeat possimus ipsa, quia quasi magni inventore omnis minima nulla deserunt impedit distinctio non pariatur dignissimos a, facere delectus quaerat suscipit reprehenderit reiciendis. Quis nemo voluptate omnis nam illum adipisci ratione, non, vero soluta aliquam perspiciatis recusandae obcaecati? Repellendus id ipsa, eius tenetur dolore officia, ad, ipsum aut quidem rem ex quia sapiente a eligendi beatae accusantium aperiam odio dolorum natus! Veniam consequatur doloribus totam incidunt repellendus recusandae laudantium nemo similique dolor, ea natus nam dolore voluptatum veritatis quos quod excepturi placeat molestiae consequuntur sunt minima?</p>
+        </app-post-excerpt>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+import AppPostExcerpt from "~/components/posts/app-post-excerpt.vue";
+
+export default {
+  components: {
+    AppPostExcerpt
+  }
+};
+</script>
+
+<style scoped>
+.all-posts-container {
+  margin-top: 1rem;
+}
+
+.posts-heading-container {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
+
---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="box">
+    <h1 class="is-size-3">Create New Post</h1>
+    <form class="form-container" @submit.prevent="handleSavePost">
+      <b-field label="Author Name">
+        <b-input type="text" v-model="edited_post.author" placeholder="Enter Author Name" />
+      </b-field>
+      <b-field label="Title">
+        <b-input type="text" v-model="edited_post.title" placeholder="Enter Title" />
+      </b-field>
+      <b-field label="Thumbnail Link">
+        <b-input
+          type="url"
+          v-model="edited_post.thumbnail_link"
+          placeholder="Enter Thumbnail Link"
+        />
+      </b-field>
+      <b-field label="Content">
+        <b-input
+          type="textarea"
+          maxlength="5000"
+          v-model="edited_post.content"
+          placeholder="Enter Title"
+        />
+      </b-field>
+      <div class="action-container">
+        <button @click="handleCancelPost" class="button is-default">Cancel</button>
+        <button type="submit" class="button is-primary">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      edited_post: {
+        author: "",
+        title: "",
+        thumbnail_link: "",
+        content: ""
+      }
+    };
+  },
+  methods: {
+    handleSavePost() {
+      console.log(this.edited_post);
+    },
+    handleCancelPost() {
+      this.$router.push("/admin");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.form-container {
+  margin-top: 1rem;
+}
+
+.action-container {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.action-container .button {
+  margin-left: 0.5rem;
+}
+</style>
+
